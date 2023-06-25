49b20cb4bd278c6f004399f0f4455baec889f0b2
isfaaghyth <isfaaghyth@gmail.com>
2023-06-25 11:44:47 +0700

    fix: change schedules description

--- src/data/schedules.js
+++ src/data/schedules.js
@@ -1,9 +1,8 @@
 export const DATA_SCHEDULES = {
-  date: "Saturday, 30 July 2022",
   time: "9:00 AM - 4:30 PM (WIB)",
-  celendar_url:
-    "https://www.google.com/calendar/render?action=TEMPLATE&text=GDG+Jakarta+DevFest+2021+-+Machine+Learning+Day&details=Hi+Devs%21+%F0%9F%99%8C%F0%9F%8F%BC%0ADevFest+is+coming%21+%F0%9F%A5%B3%0A%0AThe+world+is+changing+and+with+it+our+developer+ecosystem.+Are+you+ready+for+DevFest+2021%3F%0A%0ADevFest+2021+brings+together+thousands+of+developers+globally+for+the+largest+virtual+weekend+of+community-led+technical+learning+and+a+shared+passion+for+Google+technologies+on+Nov+26-28%21%0A%0AEvent+details+for+Machine+Learning+track%3A%0Ahttps%3A%2F%2Fyoutu.be%2FSoRgRf3B-0w%0AFriday%2C+26th+November+2021%0A%0AFind+more+at%3A%0As.id%2Fdevfest-jkt&location=https%3A%2F%2Fyoutu.be%2FSoRgRf3B-0w&dates=20211126T111500Z%2F20211126T140000Z",
-  youtube_url: "https://youtu.be/SoRgRf3B-0w",
+  date: "",
+  celendar_url: "",
+  youtube_url: "",
   agenda: [
     {
       time: "09:00 AM",
